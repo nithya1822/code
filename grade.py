97cef1bd0fdf72e1cdb007bcd0edce2cb786bf54
nitya1822 <nithyaramesh2218@gmail.com>
2026-06-23 12:06:47 +0530

    first commit

---
+++ grade.py
@@ -0,0 +1,10 @@
+def calculate_grade(marks):
+    if marks>+90:
+        return "A"
+    elif marks>=75:
+        return "B"
+    elif marks>=60:
+        return "c"
+    else:
+        return "F"
+printf(calculate_grade(5)    
